b6901ec81895d770b5ca15d03725370e95bff0cd
David Lyu <dalyu19951010@gmail.com>
2022-01-06 12:00:25 -0600

    Initial commit

---
+++ src/com/david/java/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.david.java;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        ArrayList list = new ArrayList();
+
+
+
+    
+    }
+
+    public void method() {
+
+    }
+}
